83818f37feb304c03625cebf73c4aaa4d08a0c32
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2026-05-22 10:43:08 +0200

    Arreglo de warnings y mejoras de las trazas

--- engineSrc/include/Vertex.h
+++ engineSrc/include/Vertex.h
@@ -97,6 +97,50 @@ struct Vertex {
     }
 
     /**
+ * @brief Devuelve los descriptores de atributo de vertice para Vulkan.
+ *
+ * Mapea los cinco campos del struct a las localizaciones del vertex shader:
+ * pos (0), color (1), texCoord (2), normal (3), tangent (4).
+ *
+ * @return Array de 5 VkVertexInputAttributeDescription.
+ */
+    static std::array<VkVertexInputAttributeDescription, 1> getShadowAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 1> attributeDescriptions{};
+
+        // Posicion
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof( Vertex, pos );
+
+        //// Color
+        //attributeDescriptions[1].binding = 0;
+        //attributeDescriptions[1].location = 1;
+        //attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        //attributeDescriptions[1].offset = offsetof( Vertex, color );
+
+        //// Coordenadas UV
+        //attributeDescriptions[2].binding = 0;
+        //attributeDescriptions[2].location = 2;
+        //attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        //attributeDescriptions[2].offset = offsetof( Vertex, texCoord );
+
+        //// Normal
+        //attributeDescriptions[3].binding = 0;
+        //attributeDescriptions[3].location = 3;
+        //attributeDescriptions[3].format = VK_FORMAT_R32G32B32_SFLOAT;
+        //attributeDescriptions[3].offset = offsetof( Vertex, normal );
+
+        //// Tangente
+        //attributeDescriptions[4].binding = 0;
+        //attributeDescriptions[4].location = 4;
+        //attributeDescriptions[4].format = VK_FORMAT_R32G32B32_SFLOAT;
+        //attributeDescriptions[4].offset = offsetof( Vertex, tangent );
+
+        return attributeDescriptions;
+    }
+
+    /**
      * @brief Compara dos vertices por igualdad de todos sus campos.
      *
      * Util para la deduplicacion de vertices durante la carga de mallas.
