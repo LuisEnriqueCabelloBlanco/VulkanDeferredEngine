13c5a8664de9e14fc78f7e6495264eb287a1d657
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2026-05-25 16:11:07 +0200

    bat configurado

--- utils/shader_compilation.bat
+++ utils/shader_compilation.bat
@@ -1,8 +1,25 @@
-set srcDir=%1
-set outDir=%2
+echo off
 
-for %%f in (%srcDir%shaders\*) do (
-    if %%~xf == .vert glslangValidator -I%srcDir%includes -S vert -o %outDir%%%~nf -V %%f
-    if %%~xf == .frag glslangValidator -I%srcDir%includes -S frag -o %outDir%%%~nf -V %%f
-    if %%~xf == .comp glslangValidator -I%srcDir%includes -S comp -o %outDir%%%~nf -V %%f
+set srcDir=%1
+set outDir=%srcDir%\build\
+
+echo Compilando shaders
+
+if not exist %outDir% mkdir "%outDir"
+
+for %%f in (%srcDir%\*) do (
+    if %%~xf == .vert glslangValidator -S vert -o %outDir%%%~nf -V %%f
+    if %%~xf == .frag glslangValidator -S frag -o %outDir%%%~nf -V %%f
+    if %%~xf == .comp glslangValidator -S comp -o %outDir%%%~nf -V %%f
+)
+
+echo Moviendo Shaders a los proyectos
+
+for /D %%s in (..\projects\*) do (
+
+    if not exist %%s\shaders\build\ mkdir "%%s\shaders\build\"
+
+    if not %%~ns == VulkanRenderEngine (
+        copy "%outDir%" "%%s\shaders\build\"
+    )
 )
